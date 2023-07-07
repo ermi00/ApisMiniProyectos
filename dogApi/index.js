e4343c5a94092b3cae40f4bbe38c74bb90938b22
ermi00 <miguelgonzalez.280803@gmail.com>
2023-07-07 15:54:15 -0600

    dogApi Terminado

--- dogApi/index.js
+++ dogApi/index.js
@@ -1,15 +1,20 @@
 let URL = 'https://dog.ceo/api/breeds/image/random'
 
 fetch(URL)
-    .then(res => res.json())
-    .then(res => {
-        console.log(res)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
 
         function imprimirPerro(informacion){
             let img = document.querySelector('img')
             img.src = informacion.message
         }
+
+        let boton = document.querySelector('button')
+        boton.addEventListener('click', function(){
+            location.reload(imprimirPerro(response))
+        })
         
-        imprimirPerro(res)
+        imprimirPerro(response)
     })
 
